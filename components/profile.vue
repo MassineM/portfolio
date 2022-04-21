46cc2c891c7c2a087baa22b1585c4f10d219edd6
MassineM <massine.mountasser.elv@eilco-ulco.fr>
2022-04-21 10:02:52 +0200

    :100: BETA :100:

--- components/profile.vue
+++ components/profile.vue
@@ -3,42 +3,52 @@
     <div class="mainArticle">
       <h1 class="title">Who am I?</h1>
       <div class="profiletop">
+        <div>
+          <p class="text">"Ambition is key to foul the odds"</p>
+          <p class="text">
+            ~ Massine Mountasser, 22 years old, computer science engineering
+            student
+          </p>
+          <p class="text">
+            "I believe an internship would help me discover my full potential in
+            real situations and that's fine by me, for how big my love for
+            challenge is..."
+          </p>
+          <a
+            :href="`https://drive.google.com/drive/folders/1w-tLF6T2wAaspsS4VIjSmT4pZ61an9rS?usp=sharing`"
+            target="_blank"
+            class="hiveElmnt"
+          >
+            <span v-html="link1"> </span>Link to some of my certifications...</a
+          >
+        </div>
         <img src="~/assets/ME2.png" class="pid" style="filter: saturate(0)" />
-        <p class="text">
-          "Ambition is key to foul the odds"
-          <br />
-          ~ Massine Mountasser, 22 years old, computer science engineering
-          student
-          <br />"I believe an internship would help me discover my full
-          potential in real situations and that's fine by me, for how big my
-          love for challenge is..."
-        </p>
-        <img src="~/assets/me.jpg" class="pid" />
       </div>
-      <p class="text">
-        <br />
+      <p class="text" STYLE="padding:0 0 0 5vw;">
         ~ Before highschool (till 2016) - Born in Agadir on may 13th of 1999, I
         have always managed to get myself amazed by any technology I came
         across. Playing with electronics as a child, coding mini games in early
         teenage years, enrolling to mechanic studies in highschool...
-        <br />
+      </p>
+      <p class="text" STYLE="padding:0 5vw 0 0;">
         ~ Baccalaureate (2017) - When I got my baccalaureate in mechanical techs
         and sciences, it seemed like I was destined to take a step back and
         study many more technical, scientific and litterary domains.
-        <br />
+      </p>
+      <p class="text" STYLE="padding:0 0 0 5vw;">
         ~ Preparatory classes (2017-2019) - I spent the next two years in
         preparatory classes for engineering schools - industrial technologies
         and sciences branch (CPGE TSI). Once done with all of the orientation
         and school acceptance contests, I was more than happy to join the
         Littoral Côte d'Opale's engineering school of information technologies,
         with which I still am today.
-        <br />
+      </p>
+      <p class="text" STYLE="padding:0 5vw 0 0;">
         ~ Engineering school (since 2019) - This leads us to the main purpose of
         this website I made while personally practicing web development: I'm
         still looking for an internship with some company that wants some
         profile in computer science near mine, and with whom I'd learn about
         real workspaces organisation and methods.
-        <br />
       </p>
     </div>
   </div>
@@ -47,6 +57,12 @@
 <script>
 //document.addEventListener('mousemove',function(e){mypageY=e.pageY;});
 export default {
+  data: function () {
+    return {
+      link1:
+        '<svg xmlns="http://www.w3.org/2000/svg" id="OBJECT" viewBox="0 0 512 512"><defs><style>.cls-1{fill:#bddbff}.cls-2{fill:#3d9ae2}</style></defs><rect width="497" height="350" x="7.5" y="81" class="cls-1" rx="20" ry="20"/><path d="M484.5 438.5h-457A27.54 27.54 0 0 1 0 411V101a27.54 27.54 0 0 1 27.5-27.5h457A27.54 27.54 0 0 1 512 101v310a27.54 27.54 0 0 1-27.5 27.5Zm-457-350A12.52 12.52 0 0 0 15 101v310a12.52 12.52 0 0 0 12.5 12.5h457A12.52 12.52 0 0 0 497 411V101a12.52 12.52 0 0 0-12.5-12.5Z" class="cls-2"/><path fill="#fff" d="M416 116H96a50 50 0 0 1-50 50v180a50 50 0 0 1 50 50h320a50 50 0 0 1 50-50V166a50 50 0 0 1-50-50Z"/><path d="M416 403.5H96a7.5 7.5 0 0 1-7.5-7.5A42.55 42.55 0 0 0 46 353.5a7.5 7.5 0 0 1-7.5-7.5V166a7.5 7.5 0 0 1 7.5-7.5A42.55 42.55 0 0 0 88.5 116a7.5 7.5 0 0 1 7.5-7.5h320a7.5 7.5 0 0 1 7.5 7.5 42.55 42.55 0 0 0 42.5 42.5 7.5 7.5 0 0 1 7.5 7.5v180a7.5 7.5 0 0 1-7.5 7.5 42.55 42.55 0 0 0-42.5 42.5 7.5 7.5 0 0 1-7.5 7.5Zm-313-15h306a57.61 57.61 0 0 1 49.5-49.5V173a57.61 57.61 0 0 1-49.5-49.5H103A57.61 57.61 0 0 1 53.5 173v166a57.61 57.61 0 0 1 49.5 49.5Z" class="cls-2"/><path d="m286 366-30-15-30 15V266h60v100z" class="cls-1"/><path d="M286 373.5a7.43 7.43 0 0 1-3.35-.79L256 359.39l-26.65 13.32A7.5 7.5 0 0 1 218.5 366V266a7.5 7.5 0 0 1 7.5-7.5h60a7.5 7.5 0 0 1 7.5 7.5v100a7.5 7.5 0 0 1-7.5 7.5Zm-30-30a7.43 7.43 0 0 1 3.35.79l19.15 9.57V273.5h-45v80.36l19.15-9.57a7.43 7.43 0 0 1 3.35-.79Z" class="cls-2"/><circle cx="256" cy="266" r="50" class="cls-1"/><path d="M256 323.5a57.5 57.5 0 1 1 57.5-57.5 57.56 57.56 0 0 1-57.5 57.5Zm0-100a42.5 42.5 0 1 0 42.5 42.5 42.55 42.55 0 0 0-42.5-42.5Z" class="cls-2"/><path d="M371 181H141a15 15 0 0 1 0-30h230a15 15 0 0 1 0 30Z" class="cls-1"/><path d="M371 188.5H141a22.5 22.5 0 0 1 0-45h230a22.5 22.5 0 0 1 0 45Zm-230-30a7.5 7.5 0 0 0 0 15h230a7.5 7.5 0 0 0 0-15Z" class="cls-2"/></svg>',
+    };
+  },
   methods: {},
 };
 </script>
--- components/projects.vue
+++ components/projects.vue
@@ -2,12 +2,6 @@
   <div id="projects">
     <div class="mainArticle">
       <h1 class="title">Some of my previous works:</h1>
-      <a
-        target="_blank"
-        class="pItem"
-        href="https://drive.google.com/drive/folders/1YxnYqCPUNngOJvELSOoJQK2760Orkcsb?usp=sharing"
-        >For all available documentation, check this out...</a
-      >
       <ul class="container">
         <li
           v-for="tab of projects"
@@ -47,6 +41,13 @@
           </tr>
         </table>
       </Transition>
+      <a
+        target="_blank"
+        class="hiveElmnt"
+        href="https://drive.google.com/drive/folders/1YxnYqCPUNngOJvELSOoJQK2760Orkcsb?usp=sharing"
+        ><span v-html="link2"> </span>For all available documentation, check
+        this out...</a
+      >
     </div>
   </div>
 </template>
@@ -55,6 +56,8 @@
 export default {
   data: function () {
     return {
+      link2:
+        '<svg xmlns="http://www.w3.org/2000/svg" id="OBJECT" viewBox="0 0 512 512"><defs><style>.cls-2{fill:#3d9ae2}.cls-3{fill:#fff}</style></defs><path fill="#bddbff" d="M431 432.75H269l-188 35V47.5a40 40 0 0 1 40-40h310Z"/><path d="M81 475.25a7.5 7.5 0 0 1-7.5-7.5V47.5A47.56 47.56 0 0 1 121 0h310a7.5 7.5 0 0 1 7.5 7.5v425.25a7.5 7.5 0 0 1-7.5 7.5H269.69L82.37 475.12a7 7 0 0 1-1.37.13ZM121 15a32.54 32.54 0 0 0-32.5 32.5v411.22l179.13-33.34a7 7 0 0 1 1.37-.13h154.5V15Z" class="cls-2"/><path d="M395.42 467.75A36.75 36.75 0 0 1 431 431H117.75A36.75 36.75 0 0 0 81 467.75a36.75 36.75 0 0 0 36.75 36.75H431a36.75 36.75 0 0 1-35.58-36.75Z" class="cls-3"/><path d="M431 512H117.75a44.25 44.25 0 0 1 0-88.5H431a7.52 7.52 0 0 1 .24 15 29.23 29.23 0 0 0 0 58.44 7.52 7.52 0 0 1-.24 15Zm-313.25-73.5a29.25 29.25 0 0 0 0 58.5h281.12a44.54 44.54 0 0 1 0-58.5Z" class="cls-2"/><rect width="240" height="120" x="136" y="81" class="cls-3" rx="20" ry="20"/><path d="M356 208.5H156a27.54 27.54 0 0 1-27.5-27.5v-80A27.54 27.54 0 0 1 156 73.5h200a27.54 27.54 0 0 1 27.5 27.5v80a27.54 27.54 0 0 1-27.5 27.5Zm-200-120a12.52 12.52 0 0 0-12.5 12.5v80a12.52 12.52 0 0 0 12.5 12.5h200a12.52 12.52 0 0 0 12.5-12.5v-80A12.52 12.52 0 0 0 356 88.5Z" class="cls-2"/></svg>',
       selectedTab: {},
       show: true,
       projects: [
@@ -117,7 +120,7 @@ export default {
             "Supervised personal initiative work as a part of the preparatory classes for engineering schools contests",
           tools:
             "Research and study of functionnal & technical aspects of platooning technologies on heavy vehicles",
-          link: "https://drive.google.com/drive/folders/1PnquLbU0D0tQFjlgf_Y_z-A0JCY9avii?usp=sharing",
+          link: "https://drive.google.com/file/d/1JgaUU-GB__JA06fEcK7taSBWlnE0Fk1W/view?usp=sharing",
           year: 2019,
           selected: false,
         },
